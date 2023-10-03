74b5fd7d8e3f263658707799a26c3834784606d3
brunopocay <brunopocay@gmail.com>
2023-10-03 00:15:09 -0300

    Alterações no formulario de login e register. Criado nova pagina para controle mensal e layout inicial.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 import { LoginComponent } from './Components/login/login.component';
-import { CounterComponent } from './Components/counter/counter.component';
 import { authGuard } from './Guards/auth.guard';
 import { UserIsLoggedInComponent } from './Components/user-is-logged-in/user-is-logged-in.component';
+import { ContasComponent } from './Components/contas/contas.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full', canActivate: [authGuard]},
   { path: 'login', component: LoginComponent },
   { path: 'usuario', component: UserIsLoggedInComponent, canActivate: [authGuard]},
-  { path: 'counter', component: CounterComponent, canActivate: [authGuard]},
+  { path: 'contas', component: ContasComponent, canActivate: [authGuard]},
   { path: '**', redirectTo: '/login' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './Components/nav-menu/nav-menu.component';
 import { HomeComponent } from './Components/home/home.component';
-import { CounterComponent } from './Components/counter/counter.component';
 import { LoginComponent } from '../app/Components/login/login.component';
 import { LoginDiretoComponent } from '../app/Components/login/login-direto/login-direto.component';
 import { RegisterComponent } from '../app/Components/login/register/register.component';
@@ -15,19 +14,20 @@ import { AuthInterceptor } from './Services/auth.interceptor';
 import { UserIsLoggedInComponent } from './Components/user-is-logged-in/user-is-logged-in.component';
 import { ModalLogoutComponent } from './Components/user-is-logged-in/modal-logout/modal-logout.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ContasComponent } from './Components/contas/contas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
+    HomeComponent,   
     LoginComponent,
     UserIsLoggedInComponent,
     LoginDiretoComponent,
     RegisterComponent,
     UserIsLoggedInComponent,
-    ModalLogoutComponent 
+    ModalLogoutComponent,
+    ContasComponent
   ],
   imports: [
     BrowserModule,
