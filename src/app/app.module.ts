52fca7d2fdbcce69bdd413bed6271381fdb95316
cbm <cbm.info.docs@gmail.com>
2023-11-27 16:17:48 -0300

    Correção nos module app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { CurrencyMaskModule } from 'ng2-currency-mask';
+
+import { AuthInterceptor } from './Services/auth.interceptor';
+import { CustomErrorHttpInterceptor } from './Services/StatusTextInterceptor';
 import { NavMenuComponent } from './Components/NavMenu/nav-menu.component';
 import { HomeComponent } from './Views/Home/home.component';
 import { LoginComponent } from './Views/Login/login.component';
-import { LoginDiretoComponent } from '../app/Components/login/login-direto/login-direto.component';
-import { RegisterComponent } from '../app/Components/login/register/register.component';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AuthInterceptor } from './Services/auth.interceptor';
+import { LoginDiretoComponent } from '../app/Components/Login/login-direto/login-direto.component';
+import { RegisterComponent } from '../app/Components/Login/register/register.component';
 import { UserIsLoggedInComponent } from './Components/user-is-logged-in/user-is-logged-in.component';
 import { ModalLogoutComponent } from './Components/user-is-logged-in/modal-logout/modal-logout.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ContasComponent } from './Components/contas/contas.component';
 import { RegistroMensaisComponent } from './Views/RegistrosMensais/registrosmensais.component';
-import { CustomErrorHttpInterceptor } from './Services/StatusTextInterceptor';
-import { CurrencyMaskModule } from 'ng2-currency-mask';
 import { ListRegistrosComponent } from './Components/List-registros/list-registros.component';
 
 @NgModule({
@@ -26,7 +27,6 @@ import { ListRegistrosComponent } from './Components/List-registros/list-registr
     NavMenuComponent,
     HomeComponent,
     LoginComponent,
-    UserIsLoggedInComponent,
     LoginDiretoComponent,
     RegisterComponent,
     UserIsLoggedInComponent,
